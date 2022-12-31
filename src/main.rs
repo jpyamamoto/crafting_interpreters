040e29c57322dd585395d0540375458e44dace81
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2022-12-30 19:16:43 -0600

    Chapter II: Scanning.

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+mod token;
+mod scanner;
+
+#[macro_use]
+extern crate lazy_static;
+
+use std::fs;
+use std::io::{self, BufRead, Write};
+use std::path::Path;
+use std::process::exit;
+use clap::Parser;
+use scanner::Scanner;
+use token::Token;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   file_path: Option<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if let Some(file) = args.file_path {
+        run_file(file);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(file: String) {
+    let path = Path::new(&file);
+    let contents: String = fs::read_to_string(path).expect("Unable to read file");
+    if !run(contents) {
+        exit(65);
+    }
+}
+
+fn run_prompt() {
+    print!("> ");
+    io::stdout().flush().expect("Error in REPL");
+
+    let mut stdin = io::stdin().lock().lines();
+
+    while let Some(line) = stdin.next() {
+        let instruction = line.unwrap();
+
+        if instruction.is_empty() {
+            break;
+        }
+
+        if !run(instruction) {
+            println!("Invalid command.");
+        }
+
+        print!("> ");
+        io::stdout().flush().expect("Error in REPL");
+    }
+}
+
+fn run(source: String) -> bool {
+    let mut scanner: Scanner = Scanner::from(source);
+    let tokens: &Vec<Token> = scanner.scan_tokens();
+
+    for token in tokens {
+        println!("{:?}", token.type_token);
+    }
+
+    true
+}
+
+fn error(line: usize, message: String) {
+    report(line, None, message);
+}
+
+fn report(line: usize, position: Option<String>, message: String) {
+    if let Some(pos) = position {
+        eprintln!("[line {}] Error {}: {}", line, pos, message);
+    } else {
+        eprintln!("[line {}] Error: {}", line, message);
+    }
+}
---
+++ src/token.rs
@@ -0,0 +1,23 @@
+#[derive(Clone, Debug)]
+pub enum TokenType {
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+
+    Bang, BangEqual,
+    Equal, EqualEqual,
+    Greater, GreaterEqual,
+    Less, LessEqual,
+
+    Identifier(String), String(String), Number(f64),
+
+    And, Class, Else, False, Fun, For, If, Nil, Or,
+    Print, Return, Super, This, True, Var, While,
+
+    EOF
+}
+
+pub struct Token {
+    pub type_token: TokenType,
+    pub lexeme: String,
+    pub line: usize,
+}
